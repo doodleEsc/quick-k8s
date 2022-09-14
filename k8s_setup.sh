f2069a8dc54162636cecc37f2c9d566b6b669c41
cinuor <cokie@foxmail.com>
2022-09-14 15:18:37 +0800

    fix: change apiServer

--- k8s_setup.sh
+++ k8s_setup.sh
@@ -18,7 +18,7 @@ cat << EOF | kind create cluster --name dev --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
-  apiServerAddress: "127.0.0.1"
+  apiServerAddress: "0.0.0.0"
   apiServerPort: 6443
   podSubnet: "10.244.0.0/16"
   serviceSubnet: "10.96.0.0/16"
